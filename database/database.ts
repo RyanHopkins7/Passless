7b7192b4a18a50bd8060b3abe1c6bfa4354684e0
Ashton Foley <foleyash@umich.edu>
2023-11-18 20:02:39 -0500

    SQL Queries added

---
+++ database/database.ts
@@ -0,0 +1,3 @@
+
+import mysql from "mysql2/promise";
+require('dotenv').config();
---
+++ database/queries.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE IF NOT EXISTS passless;
+
+USE passless;
+
+CREATE TABLE IF NOT EXISTS users(
+    user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    recovery_email VARCHAR(255),
+    user_name VARCHAR(255),
+    hashed_passphrase LONGTEXT,
+    master_session INT NOT NULL
+);
+
+-- Contains all passwords into single VARCHAR of the encrypted json data --
+-- User_id refers to the user the passwords belong to
+CREATE TABLE IF NOT EXISTS passwords(
+    json LONGTEXT NOT NULL,
+    user_id INT NOT NULL REFERENCES users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS sessions(
+    session_id INT NOT NULL PRIMARY KEY,
+    challenge VARBINARY(128),
+    user_id INT NOT NULL REFERENCES users(user_id)
+);
+
+ALTER TABLE users 
+ADD FOREIGN KEY (master_session) REFERENCES sessions(session_id);
