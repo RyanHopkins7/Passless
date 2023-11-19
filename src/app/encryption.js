08cd50ef88fc77676a41b718ffade233d6988a91
Joshua (Hawon) Cho <103012697+hawonc@users.noreply.github.com>
2023-11-19 08:58:49 -0500

    Update encryption.js

--- src/app/encryption.js
+++ src/app/encryption.js
@@ -6,6 +6,74 @@ function updateKey(idx, jwk){
   return
 }
 
+function getKey(idx){
+  var jwk = localStorage.getItem(idx);
+  jwk = JSON.parse(jwk)
+  if (idx == 1){
+    crypto.subtle.importKey(
+      "jwk", //can be "jwk" or "raw"
+      {   //this is an example jwk key, "raw" would be an ArrayBuffer
+          kty: "oct",
+          k: jwk.k,
+          alg: "A256GCM",
+          ext: true,
+      },
+      {   //this is the algorithm options
+          name: "AES-GCM",
+      },
+      true, //whether the key is extractable (i.e. can be used in exportKey)
+      ["encrypt", "decrypt"] //can "encrypt", "decrypt", "wrapKey", or "unwrapKey"
+    )
+    .then(function(key){
+        //returns the symmetric key
+        return(key);
+    })
+    .catch(function(err){
+        console.error(err);
+    });
+  }
+  if (idx == 2){
+    window.crypto.subtle.importKey(
+      "jwk", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
+      jwk,
+      {   //these are the algorithm options
+          name: "RSA-OAEP",
+          hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+      },
+      true, //whether the key is extractable (i.e. can be used in exportKey)
+      ["encrypt"] //"encrypt" or "wrapKey" for public key import or
+                  //"decrypt" or "unwrapKey" for private key imports
+  )
+  .then(function(publicKey){
+      //returns a publicKey (or privateKey if you are importing a private key)
+      return(publicKey);
+  })
+  .catch(function(err){
+      console.error(err);
+  });
+  }
+  if (idx == 3){
+    window.crypto.subtle.importKey(
+      "jwk", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
+      jwk,
+      {   //these are the algorithm options
+          name: "RSA-OAEP",
+          hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+      },
+      true, //whether the key is extractable (i.e. can be used in exportKey)
+      ["decrypt"] //"encrypt" or "wrapKey" for public key import or
+                  //"decrypt" or "unwrapKey" for private key imports
+  )
+  .then(function(publicKey){
+      //returns a publicKey (or privateKey if you are importing a private key)
+      return(publicKey);
+  })
+  .catch(function(err){
+      console.error(err);
+  });
+  }
+}
+
 var symkey = null
 var pubkey = null
 var privkey = null
@@ -18,6 +86,8 @@ async function generateSymKey(length = 256) {
     }, true, ['encrypt', 'decrypt']);
     // Store generated key into database
     symkey = aeskey
+    const sym = await crypto.subtle.exportKey("jwk", aeskey)
+    updateKey(1, sym)
     return {symkey}
 }
 
@@ -32,6 +102,10 @@ async function generateRsaKey(modulusLength = 2048, hash = 'SHA-256') {
       publicExponent,
       hash,
     }, true, ['encrypt', 'decrypt']);
+    const pu = await crypto.subtle.exportKey("jwk", publicKey)
+    const pr = await crypto.subtle.exportKey("jwk", privateKey)
+    updateKey(2, pu)
+    updateKey(2, pr)
     pubkey = publicKey
     privkey = privateKey
     return {
@@ -44,14 +118,11 @@ async function generateRsaKey(modulusLength = 2048, hash = 'SHA-256') {
 async function encryptSymKey(key, publkey){
     const ec = new TextEncoder();
     // Assumes that the key is passed in extracted form (jwk)
-    if (pubkey == null){
-      return 0
-    }
     if (publkey == "self"){
-        publkey = pubkey
+        publkey = getKey(2)
     }
     if (key == "self"){
-      key = symkey
+      key = getKey(1)
       
     }
 
@@ -91,12 +162,14 @@ async function decryptSymKey(buffer){
     }
     var done = await subtle.importKey("raw", buffer, "AES-GCM", true, ["encrypt", "decrypt",]);
     symkey = done
+    const sym = await crypto.subtle.exportKey("jwk", done)
+    updateKey(1, sym)
     return done;
 }
 
 async function encryptPassword(password) {
     const ec = new TextEncoder();
-    var key = symkey;
+    var key = getKey(1);
     const iv = window.crypto.getRandomValues(new Uint8Array(16));
     const ciphertext = await window.crypto.subtle.encrypt({
       name: 'AES-GCM',
@@ -110,7 +183,7 @@ async function encryptPassword(password) {
 
 async function decryptPassword(ciphertext, iv){
     const dec = new TextDecoder();
-    var key = symkey;
+    var key = getKey(1);
     const password = await window.crypto.subtle.decrypt({
         name: 'AES-GCM',
         iv,
