dc3f185683229a2b55c03a9555a9d721c111a7c2
Ryan <ryanhopk@protonmail.com>
2024-01-10 12:46:08 -0500

    Use .some to check if passphrase is empty

--- src/app/passphrase/generate/passphraseGenerator.tsx
+++ src/app/passphrase/generate/passphraseGenerator.tsx
@@ -94,7 +94,7 @@ export default function PassphraseGenerator() {
         setLoading(true);
 
         const passString = passphrase.join('-');
-        if (passString !== '-------') {
+        if (passphrase.some((w) => w === '')) {
             const enc = new TextEncoder();
 
             const keySalt = new Uint8Array(16);
