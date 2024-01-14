aa148c769ccc4bc9da857e3099e690a00647f6fd
Ryan <ryanhopk@protonmail.com>
2024-01-14 16:13:39 -0500

    Polish up the UI for uploading and downloading files

--- src/app/vault/fileVault.tsx
+++ src/app/vault/fileVault.tsx
@@ -50,100 +50,114 @@ export default function FileVault() {
     }, []);
 
     return (
-        <main>
-            <p>Upload a file</p>
-            <input type="file" onChange={async (e) => {
-                if (e.target.files?.length == 1) {
-                    const file = e.target.files[0];
-                    const fileObj = {
-                        name: file.name,
-                        type: file.type,
-                        data: Buffer.from(
-                            await file.arrayBuffer()
-                        ).toString('base64')
-                    };
+        <main className="flex justify-center">
+            <div className="w-fit my-10">
+                <div className="my-5">
+                    <h3 className="text-xl font-medium my-5">Upload a file</h3>
+                    <input type="file" onChange={async (e) => {
+                        if (e.target.files?.length == 1) {
+                            const file = e.target.files[0];
+                            const fileObj = {
+                                name: file.name,
+                                type: file.type,
+                                data: Buffer.from(
+                                    await file.arrayBuffer()
+                                ).toString('base64')
+                            };
 
-                    setFileData(JSON.stringify(fileObj));
-                }
-            }}></input>
-            <button className="hover:underline" onClick={async () => {
-                if (
-                    vaultKey !== undefined
-                    && fileData !== null
-                ) {
-                    const enc = new TextEncoder();
-                    const iv = randomBytes(16);
-                    const fileCt = await window.crypto.subtle.encrypt(
-                        {
-                            name: 'AES-GCM',
-                            iv: iv
-                        },
-                        vaultKey,
-                        enc.encode(fileData)
-                    );
-
-                    const res = await fetch('/api/vault/file', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify({
-                            data: Buffer.from(fileCt).toString('base64'),
-                            iv: bytesToHex(iv)
-                        })
-                    });
-
-                    if (res.status === 201) {
-                        alert('File upload successful');
-                    } else {
-                        alert('Error: file upload was not successful');
-                    }
-                }
-            }}>Submit</button>
-
-            <p>Download the file that you've uploaded</p>
-            <button className="hover:underline" onClick={async () => {
-                if (vaultKey !== undefined) {
-                    const res = await fetch('/api/vault/file');
-
-                    if (res.status === 404) {
-                        alert('Error: you must upload a file first');
-                    } else if (res.status !== 200) {
-                        alert('Error: file download was not successful');
-                    } else {
-                        const resJson = await res.json();
-                        const iv = hexToBytes(resJson.iv);
-                        const fileCt = Buffer.from(resJson.data, 'base64');
-                        const dec = new TextDecoder();
-
-                        const fileObj = JSON.parse(
-                            dec.decode(
-                                await window.crypto.subtle.decrypt(
+                            setFileData(JSON.stringify(fileObj));
+                        }
+                    }}></input>
+                    <button
+                        className="button bg-dark-purple mx-3 px-6 py-2 w-fit h-fit rounded-3xl text-white font-bold text-center"
+                        onClick={async () => {
+                            if (
+                                vaultKey !== undefined
+                                && fileData !== null
+                            ) {
+                                const enc = new TextEncoder();
+                                const iv = randomBytes(16);
+                                const fileCt = await window.crypto.subtle.encrypt(
                                     {
                                         name: 'AES-GCM',
                                         iv: iv
                                     },
                                     vaultKey,
-                                    fileCt
-                                )
-                            )
-                        );
+                                    enc.encode(fileData)
+                                );
 
-                        const a = window.document.createElement('a');
-                        a.href = window.URL.createObjectURL(
-                            new Blob(
-                                [Buffer.from(fileObj.data, 'base64')],
-                                { type: fileObj.type }
-                            )
-                        );
-                        a.download = fileObj.name;
-                        
-                        document.body.appendChild(a);
-                        a.click();
-                        document.body.removeChild(a);
-                    }
-                }
-            }}>Download file</button>
+                                const res = await fetch('/api/vault/file', {
+                                    method: 'POST',
+                                    headers: {
+                                        'Content-Type': 'application/json'
+                                    },
+                                    body: JSON.stringify({
+                                        data: Buffer.from(fileCt).toString('base64'),
+                                        iv: bytesToHex(iv)
+                                    })
+                                });
+
+                                if (res.status === 201) {
+                                    alert('File upload successful');
+                                } else {
+                                    alert('Error: file upload was not successful');
+                                }
+                            }
+                        }}>Submit</button>
+                </div>
+
+                <div>
+                    <h3 className="text-xl font-medium my-5">
+                        Download the file that you've uploaded
+                    </h3>
+                    <div className="flex justify-center">
+                        <button
+                            className="block button bg-dark-purple mx-3 px-6 py-2 w-fit h-fit rounded-3xl text-white font-bold text-center"
+                            onClick={async () => {
+                                if (vaultKey !== undefined) {
+                                    const res = await fetch('/api/vault/file');
+
+                                    if (res.status === 404) {
+                                        alert('Error: you must upload a file first');
+                                    } else if (res.status !== 200) {
+                                        alert('Error: file download was not successful');
+                                    } else {
+                                        const resJson = await res.json();
+                                        const iv = hexToBytes(resJson.iv);
+                                        const fileCt = Buffer.from(resJson.data, 'base64');
+                                        const dec = new TextDecoder();
+
+                                        const fileObj = JSON.parse(
+                                            dec.decode(
+                                                await window.crypto.subtle.decrypt(
+                                                    {
+                                                        name: 'AES-GCM',
+                                                        iv: iv
+                                                    },
+                                                    vaultKey,
+                                                    fileCt
+                                                )
+                                            )
+                                        );
+
+                                        const a = window.document.createElement('a');
+                                        a.href = window.URL.createObjectURL(
+                                            new Blob(
+                                                [Buffer.from(fileObj.data, 'base64')],
+                                                { type: fileObj.type }
+                                            )
+                                        );
+                                        a.download = fileObj.name;
+
+                                        document.body.appendChild(a);
+                                        a.click();
+                                        document.body.removeChild(a);
+                                    }
+                                }
+                            }}>Download file</button>
+                    </div>
+                </div>
+            </div>
         </main >
     );
 }
