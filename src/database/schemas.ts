783711a988e69731db6a74c503abe6da2313ee23
Ryan <ryanhopk@protonmail.com>
2024-01-14 21:24:19 -0500

    Connect to passless db

--- src/database/schemas.ts
+++ src/database/schemas.ts
@@ -1,6 +1,6 @@
 import mongoose, { Types } from "mongoose";
 
-mongoose.connect('mongodb://localhost:27017');
+mongoose.connect('mongodb://localhost:27017/passless');
 
 export interface IWebAuthenticator {};
 export interface IDevice {
